f3a6a258bc65594cc759d12664cdebb7e4a42142
Davann CR <davannsmart9@gmail.com>
2022-11-12 18:32:58 +0700

    1.0.4

--- js/windowScroll.js
+++ js/windowScroll.js
@@ -1,15 +1,20 @@
+var bodys=document.querySelector('.body')
 var prePos=window.pageYOffset;
 window.onscroll=function(){
     var newPos=window.pageYOffset;
-    if(prePos>newPos){
+    if(prePos>=newPos){
         document.querySelector('.header-bar').style.top="0";
     }else{
-        if(prePos>300){
+        if(prePos>200){
         document.querySelector('.header-bar').style.top="-127px";            
         }
-
     }
     prePos=newPos;
+    /////////////////////////
+    var statePos=window.pageYOffset;
+    if(statePos>100){
+        bodys.classList.add('bodys');
+    }
 }
 window.onreset=function(){
     document.querySelector('.header-bar').style.top="0";
