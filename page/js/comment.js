a2e2b03b3810122be88bdffa7db6562f051c7370
Davann CR <davannsmart9@gmail.com>
2022-11-28 21:23:43 +0700

    1.3.5

--- page/js/comment.js
+++ page/js/comment.js
@@ -1,130 +1,71 @@
+var recom="<i class='fa fa-thumbs-up' aria-hidden='true'></i>Recommended"
+var notrecom="<i class='fa fa-thumbs-down' aria-hidden='true'></i>Not recommended"
 function commentTo(data){
     var stars
+    var recomnots
     if(data.numStars==1){
+        recomnots=notrecom
         stars = "<span class='fa fa-star checked'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span>"
     }else if(data.numStars==2){
+        recomnots=notrecom
         stars="<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span>"
     }else if(data.numStars==3){
+        recomnots=recom
         stars="<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star'></span><span class='fa fa-star'></span>"
     }else if(data.numStars==4){
+        recomnots=recom
         stars="<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star'></span>"
     }else if(data.numStars==5){
+        recomnots=recom
         stars="<span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span>"
     }else{
+        recomnots=notrecom
         stars = "<span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><span class='fa fa-star'></span>"
     }
-    var html="<div class='commented'><div class='topcomment'><img src='../image/user.png' width='40px'><label class='name'><label>"+data.name+"<br>"+data.email+"</label></label><label><br>"+stars+"</label><label class='recomnot'><br><label>"+data.recomnots+"</label></label><label><br><label>"+data.current+"</label></label></div><div class='bottoncomment'>"+data.comment+"</div></div>"
+    var html="<div class='commented'><div class='topcomment'><img src='../image/user.png' width='40px'><label class='name'><label>"+data.name+"<br>"+data.email+"</label></label><label><br>"+stars+"</label><label class='recomnot'><br><label>"+recomnots+"</label></label><label><br><label>"+data.current+"</label></label></div><div class='bottoncomment'>"+data.comment+"</div></div>"
     $('#getPost').append(html)
 }
-var numStar
-var recomnot="<i class='fa fa-thumbs-down' aria-hidden='true'></i>Not recommended"
+function readData(){
+    fetch('https://script.google.com/macros/s/AKfycbw9BUMRUsAj9PoBBjtQRbOcJ5sQAnaLBLX9HPfCw-cglXHGZlfKXgaz_twYlWRmIv5B/exec')
+    .then(res=>res.json()).then(data=>{
+        // console.log(data.content.length);
+        document.querySelector('#numRecomment').innerHTML=data.content.length-1
+        let tData=[]
+        for(var i=1; i<data.content.length;i++){
+            let getData={'name':'','email':'','numStars':0,'comment':'','current':''}
+            getData.name=data.content[i][0]
+            getData.email=data.content[i][1]
+            getData.numStars=data.content[i][2]
+            getData.comment=data.content[i][3]
+            getData.current=data.content[i][4]
+            tData[i-1]=getData
+        }
+        for(var j=tData.length-1;j>=0;j--) {
+            commentTo(tData[j])
+        }
+    })
+}
+readData()
+var i=1;
 $(document).ready(function() {
-    var recom="<i class='fa fa-thumbs-up' aria-hidden='true'></i>Recommended"
-    var notrecom="<i class='fa fa-thumbs-down' aria-hidden='true'></i>Not recommended"
-    $('#star1').click(function(){
-        document.querySelector('#ratingRecomment').innerHTML=notrecom
-        document.querySelector('#star1').style.color = 'orange';
-        document.querySelector('#star2').style.color = 'white';
-        document.querySelector('#star3').style.color = 'white';
-        document.querySelector('#star4').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        numStar=1
-        recomnot=notrecom
-    })
-    $('#star2').click(function(){
-        document.querySelector('#ratingRecomment').innerHTML=notrecom
-        document.querySelector('#star1').style.color = 'orange';
-        document.querySelector('#star2').style.color = 'orange';
-        document.querySelector('#star3').style.color = 'white';
-        document.querySelector('#star4').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        numStar=2
-        recomnot=notrecom
-    })
-    $('#star3').click(function(){
-        document.querySelector('#ratingRecomment').innerHTML=recom
-        document.querySelector('#star1').style.color = 'orange';
-        document.querySelector('#star2').style.color = 'orange';
-        document.querySelector('#star3').style.color = 'orange';
-        document.querySelector('#star4').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        numStar=3
-        recomnot=recom
-    })
-    $('#star4').click(function(){
-        document.querySelector('#ratingRecomment').innerHTML=recom
-        document.querySelector('#star1').style.color = 'orange';
-        document.querySelector('#star2').style.color = 'orange';
-        document.querySelector('#star3').style.color = 'orange';
-        document.querySelector('#star4').style.color = 'orange';
-        document.querySelector('#star5').style.color = 'white';
-        numStar=4
-        recomnot=recom
-    })
-    $('#star5').click(function(){
-        document.querySelector('#ratingRecomment').innerHTML=recom
-        document.querySelector('#star1').style.color = 'orange';
-        document.querySelector('#star2').style.color = 'orange';
-        document.querySelector('#star3').style.color = 'orange';
-        document.querySelector('#star4').style.color = 'orange';
-        document.querySelector('#star5').style.color = 'orange';
-        numStar=5
-        recomnot=recom
-    })
-
-    var data=[]
-    if(!localStorage.dataRestaurant1){
-        localStorage.dataRestaurant1=[]
-    }else{
-        data=JSON.parse(localStorage.dataRestaurant1)
-    }
-    document.querySelector('#numRecomment').innerHTML=data.length
-    for(var i=data.length-1;i>=0;i--){
-        commentTo(data[i])
-    }
     $('#post').click(function(){
-        var currentDate=new Date();
-        var day=currentDate.getDate();
-        var month=currentDate.getMonth()+1;
-        var year=currentDate.getFullYear();
-        var hour=currentDate.getHours();
-        var minute=currentDate.getMinutes();
-        var second=currentDate.getSeconds();
-        var current=day+'/'+month+'/'+year+'  '+hour+':'+minute+':'+second
-        document.querySelector('#eror').innerHTML=''
-        if($('#username').val()!=''&&$('#email').val()!=''&&$('#comment').val()!=''){
-        var info={
-            'name':$('#username').val(),
-            'email':$('#email').val(),
-            'comment':$('#comment').val(),
-            'current':current+'',
-            'numStars':numStar,
-            'recomnots':recomnot+''
-        }
-        data.push(info)
-        localStorage.dataRestaurant1=JSON.stringify(data)
-        commentTo(info)
-        $('#username').val('')
-        $('#email').val('')
-        $('#comment').val('')     
-        document.querySelector('#star1').style.color = 'white';
-        document.querySelector('#star2').style.color = 'white';
-        document.querySelector('#star3').style.color = 'white';
-        document.querySelector('#star4').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        document.querySelector('#star5').style.color = 'white';
-        document.querySelector('#getPost').innerHTML = ''
-        for(var i=data.length-1;i>=0;i--){
-            commentTo(data[i])
-        }
-        document.querySelector('#ratingRecomment').innerHTML=''
-        numStar=0
-        recomnot=notrecom
-        document.querySelector('#numRecomment').innerHTML=data.length
-        document.querySelector('#ratingRecomment').innerHTML='&nbsp;'
+        if(i%2==0){
+            document.querySelector('#logoRefresh').style.transform='rotate(-1000deg)'            
         }else{
-            document.querySelector('#eror').innerHTML='Please complete all!'
+            document.querySelector('#logoRefresh').style.transform='rotate(1000deg)'            
         }
+        i++
+        document.querySelector('#getPost').innerHTML=''
+        readData()
+    })
+    $('#refresh').click(function(){
+        if(i%2==0){
+            document.querySelector('#logoRefresh').style.transform='rotate(-1000deg)'            
+        }else{
+            document.querySelector('#logoRefresh').style.transform='rotate(1000deg)'            
+        }
+        i++
+        document.querySelector('#getPost').innerHTML=''
+        readData()
     })
 })
--- page/js/sheetDB.js
+++ page/js/sheetDB.js
@@ -2,5 +2,5 @@ let myform=document.querySelector('form');
 myform.addEventListener('submit',(e)=>{
     e.preventDefault();
     let mydataform=new FormData(myform);
-    fetch('https://script.google.com/macros/s/AKfycbxuGPx8V2_2OGfj91d2L7kCzDhKHfDDUqQ5j2kQOWwPpcw-2G7w0IcgsD_JQkXRBfep/exec',{method:'POST',body:mydataform})
+    fetch('https://script.google.com/macros/s/AKfycbzPdG4zWGNHU-NDystbIiVjWRYYW56QcEU2t-Hr4TRzqxAkDn-efK_WIUDh0ghpYZ54/exec',{method:'POST',body:mydataform})
 })
