6df997862495ae2246cd00692fc7dd346554aa1f
Davann CR <davannsmart9@gmail.com>
2022-12-07 16:21:01 +0700

    1.4.5

---
+++ js/signin.js
@@ -0,0 +1,39 @@
+
+fetch('https://script.google.com/macros/s/AKfycbx4mVyh9NAA_VZCFTfnhWhtpUy21IbxLbAUCq8MoIsMUjRLvqNHiMO5zpgpcKXjhGKh/exec')
+.then(res=>res.json()).then(data=>{
+    let ld=document.querySelector('#loginData')
+    let eml=document.querySelector('#email')
+    let psw=document.querySelector('#password')
+    let myData=[]
+    let obj={firstname:'',lastname:'',email:'',password:''};
+    for(let i=1;i<data.content.length;i++){
+        obj.firstname=data.content[i][0];
+        obj.lastname=data.content[i][1];
+        obj.email=data.content[i][2];
+        obj.password=data.content[i][3];
+        myData[i-1]=obj
+   
+    }
+    // localStorage.yourData=JSON.stringify(obj);
+    ld.addEventListener('click',function(){
+        for(let i=0;i<myData.length;i++){
+            if(obj.email==eml.value){
+                localStorage.yourData=JSON.stringify(obj);
+                window.location.assign("https://davanncr.github.io/Project-Final/")
+                alert("Success")
+            }else{
+                alert('something wrong!')
+            }
+        }
+    
+    })
+})
+
+
+
+
+
+
+
+
+
---
+++ js/signup.js
@@ -0,0 +1,38 @@
+var flg=document.querySelector('#formLogin')
+var btn=document.querySelector('.btn')
+var flg=document.querySelector('#formLogin')
+var fsg=document.querySelector('#formSignup')
+let myform=document.querySelector('#formSignup')
+myform.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    fdata=new FormData(myform)
+    let fpw=document.querySelector('#passwords')
+    let fpws=document.querySelector('#repasswords')
+    let chbox=document.getElementById('agree')
+    let fname=document.getElementById('firstnames')
+    let lname=document.getElementById('lasstnames')
+    let email=document.getElementById('emails')
+    if(chbox.checked){
+        if(fpw.length<8){
+            alert('Password must less than 8 characters!')
+        }else{
+            if(fpw.value==fpws.value){
+                fetch('https://script.google.com/macros/s/AKfycbxrG2lt0oVFNzR8RXeYyJWfaGimEkaaglcg1-HY2vFjRk6F6UeFt4Ge0U1JoPh9riQ1/exec',{method:'POST',body:fdata})   
+                btn.style.left='0px'
+                flg.style.left='0px'
+                fsg.style.left='400px'
+                fname.value=''
+                lname.value=''
+                email.value=''
+                fpw.value=''
+                fpws.value=''
+                chbox.checked=false
+            }else{
+                alert('Password not match!');
+            }        
+        }        
+    }else{
+        alert('You must agree!')
+    }
+    
+})
