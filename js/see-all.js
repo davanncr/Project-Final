86df57b594b57531118d9530fc6e5f480313a935
Jammie-Dev779 <vinunsombath@gmail.com>
2022-12-14 22:56:07 +0700

    1.5.2

--- js/see-all.js
+++ js/see-all.js
@@ -5,71 +5,81 @@ const button4 = document.querySelector("#see-more-4").addEventListener("click",s
 const button5 = document.querySelector("#see-more-5").addEventListener("click",seeAll5)
 const button6 = document.querySelector("#see-more-6").addEventListener("click",seeAll6)
 
-const seeless1 = document.querySelector("#seeless1")
-const seeless2 = document.querySelector("#seeless2")
+const seeless1 = document.querySelector("#seeless1").addEventListener("click",seeLess1)
+const seeless2 = document.querySelector("#seeless2").addEventListener("click",seeLess2)
+const seeless3 = document.querySelector("#seeless3").addEventListener("click",seeLess3)
+const seeless4 = document.querySelector("#seeless4").addEventListener("click",seeLess4)
+const seeless5 = document.querySelector("#seeless5").addEventListener("click",seeLess5)
+const seeless6 = document.querySelector("#seeless6").addEventListener("click",seeLess6)
 
-let j = 0
 
 function seeAll1(){
     style(1)
-    create_button_seeless(1)
 }
+function seeLess1(){
+    style_see_less(1)
+}
+
 
 function seeAll2(){
     style(2)
-    create_button_seeless(2)
 }
+function seeLess2(){
+    style_see_less(2)
+}
+
 
 function seeAll3(){
     style(3)
-    create_button_seeless(3)
 }
+function seeLess3(){
+    style_see_less(3)
+}
+
 
 function seeAll4(){
     style(4)
-    create_button_seeless(4)
 }
+function seeLess4(){
+    style_see_less(4)
+}
+
 
 function seeAll5(){
     style(5)
-    create_button_seeless(5)
 }
+function seeLess5(){
+    style_see_less(5)
+}
+
 
 function seeAll6(){
     style(6)
-    create_button_seeless(6)
 }
+function seeLess6(){
+    style_see_less(6)
+}
+
 
 function style(i){
     let contents = document.getElementsByClassName(`restaurant${i}`)
+    let button = document.querySelector(`#see-more-${i}`)
+        button.style.display = "none"
+    let seeless = document.querySelector(`#seeless${i}`)
+        seeless.style.display = "" 
         contents[0].style.flexWrap = "wrap"
         contents[0].style.overflow = "hidden"
         contents[0].style.height = "33rem"
 }
 function style_see_less(i){
     let contents = document.getElementsByClassName(`restaurant${i}`)
+    let seeless = document.querySelector(`#seeless${i}`)
+        seeless.style.display = "none" 
+    let button = document.querySelector(`#see-more-${i}`)
+        button.style.display = ""
         contents[0].style.flexWrap = "nowrap"
         contents[0].style.overflow = "auto"
         contents[0].style.height = "18.75rem"
-    let remove = document.querySelector(`#seeless${i}`)
-        remove.remove()
 }
 
-function create_button_seeless(i){
-    const seeless = document.createElement("button")
-    const restaurant = document.querySelector(`#category${i}`)
-    restaurant.append(seeless)
-    seeless.classList.add("see-more","seeless")
-    seeless.setAttribute("id",`seeless${i}`)
-    seeless.innerText = "see less"
-    seeless.style.width = "87vw"
-    seeless.style.marginLeft = "auto"
-    seeless.style.opacity = "0.5"
-    j = i
-    seeless.addEventListener("click",see_less)
-}
-
-function see_less(){
-    style_see_less(j)
-}
 
