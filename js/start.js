141a28952273d6f1b6dc549538b8c8766db626c9
Davann CR <davannsmart9@gmail.com>
2022-12-16 19:40:20 +0700

    1.6.5

--- js/start.js
+++ js/start.js
@@ -5,6 +5,6 @@ let wLog=document.querySelector('#wantLogin');
         forLog.innerHTML = "<button class='login' id='wantLogin' onclick='goLoging()'><a>Log In</a></button>"
     }else{
         let getData=JSON.parse(localStorage.yourData)
-        forLog.innerHTML="<label class='yourname' onclick='goLoging()'>"+getData.firstname+" "+getData.lastname+"<label>"
+        forLog.innerHTML=`<img src="image/user1.png" class='yourname' width="50px" onclick='goLoging()'>`
     }
 
